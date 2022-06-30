6ec16fd6f1baa28c55a36ccdc641ec837e0f61e7
Michelle <clairsin@gmail.com>
2022-06-30 12:46:11 -0400

    commit tree 25d8966f4fce7a1e10dbee93a08d4c5453de391f

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,45 @@
+class SpicesController < ApplicationController
+
+def index
+  spices = Spice.all
+  render json: spices, status: :ok
+end
+
+def show
+  spice = Spice.find_by(id: params[:id])
+  if spice
+    render json: spice, status: :ok
+  else
+    render json: {error: "Spice not found"}, status: :not_found
+  end
+end
+
+def create
+  spice = Spice.create(spice_params)
+  render json: spice, status: :created
+end
+
+def update
+  spice = Spice.find_by(id: params[:id])
+  if spice
+    spice.update(spice_params)
+    render json: spice, status: :accepted
+  else
+    render json: {error: "Spice not found"}, status: :not_found
+  end
+end
+
+def destroy
+  spice = Spice.find_by(id: params[:id])
+  spice.destroy
+  head :no_content
+end
+
+
+private
+
+def spice_params
+  params.permit(:title, :image, :description, :notes, :rating)
+end
+
+end
